d5aa40dc2a6374bb8ddae406df27ae5c1b36032c
MoonTahoe <alex@moonhighway.com>
2019-04-22 19:52:13 -0700

    resolvers and context that supports me, authorize, and createAccount

---
+++ api/src/context.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require("mongodb");
+const { PubSub } = require("apollo-server-express");
+const jwt = require("jsonwebtoken");
+
+const pubsub = new PubSub();
+pubsub.ee.setMaxListeners(500);
+
+const createContext = async () => {
+  if (!process.env.MONGODB_URI) {
+    throw new Error(
+      `MONGODB_URI undefined. Please provide this environment variable`
+    );
+  }
+
+  if (!process.env.AUTH_SECRET) {
+    throw new Error(
+      `AUTH_SECRET undefined. Please provide this environment variable`
+    );
+  }
+
+  const client = await MongoClient.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  });
+  const db = client.db();
+
+  return async ({ req }) => {
+    let currentUser = null;
+    const applicants = db.collection("applicants");
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.replace(/(B|b)earer/, "").trim();
+      const { email } = jwt.verify(token, process.env.AUTH_SECRET);
+      currentUser = await applicants.findOne({ email });
+    }
+    return {
+      applicants,
+      currentUser
+    };
+  };
+};
+
+module.exports = { createContext };
--- api/src/index.js
+++ api/src/index.js
@@ -2,31 +2,27 @@ const express = require("express");
 const expressPlayground = require("graphql-playground-middleware-express")
   .default;
 const { readFileSync } = require("fs");
-const { ApolloServer, PubSub } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 const { createServer } = require("http");
+const { createContext } = require("./context");
+const resolvers = require("./resolvers");
 const path = require("path");
 
 const typeDefs = readFileSync(path.join(__dirname, "schema.graphql"), "UTF-8");
 
 const start = async port => {
-  const pubsub = new PubSub();
-  pubsub.ee.setMaxListeners(500);
-
-  const context = ({ req, connection }) => ({});
-
+  const context = await createContext();
   const server = new ApolloServer({
     typeDefs,
     context,
+    resolvers,
     introspection: true,
-    mocks: true
+    mocks: true,
+    mockEntireSchema: false
   });
 
   const app = express();
-
-  app.use(express.static(path.join(__dirname, "..", "client", "build")));
-
-  server.applyMiddleware({ app });
-
+  app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
   app.get(
     "/playground",
     expressPlayground({
@@ -35,8 +31,10 @@ const start = async port => {
     })
   );
 
+  server.applyMiddleware({ app });
   const httpServer = createServer(app);
   server.installSubscriptionHandlers(httpServer);
+
   httpServer.listen({ port }, () => {
     console.log(`GraphQL API running on port ${port}`);
   });
